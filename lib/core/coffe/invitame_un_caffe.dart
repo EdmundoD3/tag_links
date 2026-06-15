7583f277db3c2d58b9f5b1f611b4edc9073de932
Edmundo D3 <edmundorm1996@gmail.com>
2026-06-15 02:06:19 -0600

    se agrego buy me coffe

--- lib/core/coffe/invitame_un_caffe.dart
+++ lib/core/coffe/invitame_un_caffe.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:tag_links/core/locate/t_keys.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-final String _donationUrl ="";
+final Uri _donationUrl = Uri.parse('https://buymeacoffee.com/Notita');
+
 class InvitameUnCaffe extends ConsumerWidget {
   const InvitameUnCaffe({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final theme = Theme.of(context);
-    if(_donationUrl.isEmpty) return const SizedBox.shrink();
-    
+
     return ListTile(
       leading: const Icon(Icons.coffee, color: Color(0xFFBB9457)),
       title: Text(
@@ -18,12 +19,21 @@ class InvitameUnCaffe extends ConsumerWidget {
         style: TextStyle(color: theme.textTheme.bodyMedium?.color),
       ),
       subtitle: Text(
-        ref.tr(TKeys.ads.buyCoffeeDesc, fallback: 'Si te gusta la app, invítame un café para apoyar el proyecto'),
+        ref.tr(
+          TKeys.ads.buyCoffeeDesc,
+          fallback:
+              'Si te gusta la app, invítame un café para apoyar el proyecto',
+        ),
         style: TextStyle(color: theme.hintColor),
       ),
       onTap: _launchDonationUrl,
     );
   }
 
-  void _launchDonationUrl() {}
+  Future<void> _launchDonationUrl() async {
+    await launchUrl(
+      _donationUrl,
+      mode: LaunchMode.externalApplication,
+    );
+  }
 }
